ffa2db448d9aea0eba1d041102a8863ebb2db803
Yi Feng Zhang <yifengz246@gmail.com>
2025-05-20 18:08:40 -0400

    Update Part(b).java

--- Q4/Part(b).java
+++ Q4/Part(b).java
@@ -1,1 +1,14 @@
-
+ public boolean clearPair(int row, int col){
+        int puz = puzzle[row][col];
+        for(int rows = 0; rows < puzzle.length; row++){
+            for(int cols = 1; cols < puzzle[rows].length-1;cols++){
+                if(puzzle[rows][cols-1] + puz == 10 || puzzle[row][col+1] + puz == 10 ){
+                    puzzle[row][col] = 0;
+                    puzzle[rows][cols] = 0;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
