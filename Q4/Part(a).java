4cc0b943605a303392b9491556eaaea013a9f7b5
Yi Feng Zhang <yifengz246@gmail.com>
2025-05-20 18:08:27 -0400

    Update Part(a).java

--- Q4/Part(a).java
+++ Q4/Part(a).java
@@ -1,1 +1,12 @@
+class SumOrSameGamejava{
+    private int[][] puzzle;
 
+    public SumOrSameGamejava(int numRows, int numCols){
+        int[][] puzzle = new int[numRows][int numCols];
+        int rand = (int) (Math.random()*9)+1;
+        for(int row = 0; row < puzzle.length; row++){
+            for(int col = 0; col < puzzle[row].length){
+                puzzle[row][col] = rand;
+            }
+        }
+    }
