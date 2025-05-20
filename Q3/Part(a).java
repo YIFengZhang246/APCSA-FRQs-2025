a248ebafd743c9c928f59402ce234cf52d69a16a
Yi Feng Zhang <yifengz246@gmail.com>
2025-05-20 18:03:40 -0400

    Update Part(a).java

--- Q3/Part(a).java
+++ Q3/Part(a).java
@@ -1,1 +1,7 @@
-
+public Round(String[] names) {
+    competitorList = new ArrayList<Competitor>();
+    for (int i = 0; i < names.length; i++) {
+        Competitor comp = new Competitor(names[i], i + 1);
+        competitorList.add(comp);
+    }
+}
